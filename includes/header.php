3344e0052fb89fdeba5fa87d73819ad39cc990c0
Daan Swart <daswart18@outlook.com>
2023-09-05 14:52:07 +0200

    add links to login and sign up page

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,8 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
+    <!-- My stylesheet -->
+    <link rel="stylesheet" href="includes/css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
     <!--Let browser know website is optimized for mobile-->
@@ -17,30 +19,34 @@
 
 <body>
     <header>
+
         <nav class="nav-wrapper green">
             <div class="container">
+
+                <!-- Navbar -->
                 <a href="index.php" class="brand-logo">Rent-A-Car</a>
                 <a href="" class="sidenav-trigger white-text" data-target="mobile-menu">
                     <i class="material-icons">menu</i>
                 </a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="../login.php" class="btn green darken-2">Inloggen</a></li>
-                    <li><a href="add.php" class="btn green darken-3">Registreren</a></li>
+                    <li><a href="login.php" class="btn green darken-2">Inloggen</a></li>
+                    <li><a href="sign_up.php" class="btn green darken-3">Registreren</a></li>
                 </ul>
                 <ul class="right hide-on-med-and-down" id="firstnav">
-                    <li><a href="#photo's" id="home-link">Home</a></li>
+                    <li><a href="index.php" id="home-link">Home</a></li>
                     <li><a href="#services">Auto huren</a></li>
                     <li><a href="busjeshuren.php">Bus huren</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
 
+                <!-- Sidenav -->
                 <ul class="sidenav grey lighten-2" id="mobile-menu">
                     <li><a href="index.php">Home</a></li>
                     <li><a href="#">Auto Huren</a></li>
                     <li><a href="busjeshuren.php">Bus huren</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#services">Inloggen</a></li>
-                    <li><a href="#contact">Registreren</a></li>
+                    <li><a href="login.php">Inloggen</a></li>
+                    <li><a href="sign_up.php">Registreren</a></li>
                 </ul>
             </div>
         </nav>
