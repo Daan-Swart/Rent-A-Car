ac6f3c139f4faa7f88ef0e4ea02738127110009b
Daan Swart <daswart18@outlook.com>
2023-09-06 13:16:41 +0200

    Create database.php

---
+++ includes/database.php
@@ -0,0 +1,15 @@
+<?php
+    // Database connection
+   $host = "localhost";
+   $dbname = "rent-a-car";
+   $username = "root";
+   $password = "";
+   
+   $mysqli = new mysqli($host, $username, $password, $dbname);
+
+
+   if($mysqli->connect_errno){
+        die("Connection error: " . $mysqli->connect_error );
+   }
+   return $mysqli;
+?>
