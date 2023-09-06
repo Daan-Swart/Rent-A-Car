34bbed74a2bfcc49978c4d1535c9758b96d84ff6
Daan Swart <daswart18@outlook.com>
2023-09-06 13:16:50 +0200

    Update process_signup.php

--- includes/process_signup.php
+++ includes/process_signup.php
@@ -1,38 +1,64 @@
 <?php
 
-// go back to register (temporay code) 
-echo '<a href="../sign_up.php"><<< Naar registreren</a>';
-echo "<br><br>";
-
 // name validation
-if (empty($_POST['name'])){
+if (empty($_POST['name'])) {
     die('name is required');
 }
 
 // email validation
-if(! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is re  quired");
 }
 
 //PASSWORD VALIDATION
 
 //Check if password is at least 8 characters
-if(strlen($_POST["password"]) < 8){
+if (strlen($_POST["password"]) < 8) {
     die("password must be at least 8 characters");
-
 }
 
 //Check if password containes at least 1 letter
-if(! preg_match("/[a-z]/i", $_POST["password"])){
+if (!preg_match("/[a-z]/i", $_POST["password"])) {
     die('password must contain at least one letter');
 }
 
 //Check if password containes a number
-if (! preg_match("/[0-9]/", $_POST["password"])) {
+if (!preg_match("/[0-9]/", $_POST["password"])) {
     die('password must contain at least one number');
 }
 
+//Check if password and repeated password are the same
+if ($_POST['password'] !== $_POST["password_confirmation"]) {
+    die("Passwords must match");
+}
+
+//Hash the password
+$password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+//Database connection
+$mysqli = require __DIR__ . "/database.php";
+
+// insert user credentials into database table user. 
+$sql = "INSERT INTO user (name, email, password_hash) VALUES (?, ?, ?)";
+$stmt = $mysqli->stmt_init();
+if (!$stmt->prepare($sql)) {
+    die("SQL error:" . $mysqli->error);
+}
+
+$stmt->bind_param("sss", $_POST['name'], $_POST['email'], $password_hash);
+if ($stmt->execute()) {
+    header("Location: ../signup_succes.php");
+    mysqli_close($mysqli);
+    exit;
+} else {
+    if ($mysqli->errno === 1062) {
+        die("email already taken");
+    }
+    die($mysqli->error . " " . $mysqli->errno);
+}
 
 
-//print post array (temporay code)
-print_r($_POST);
+//(temporay code)
+// print_r($_POST);
+// echo "<br>";
+// var_dump($password_hash);
