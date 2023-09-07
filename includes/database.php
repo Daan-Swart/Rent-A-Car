51628978a784d199f41687f5138a7cad8839952c
Daan Swart <daswart18@outlook.com>
2023-09-07 12:59:09 +0200

    check email

--- includes/database.php
+++ includes/database.php
@@ -3,13 +3,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "rent-a-ar";
+$dbname = "rent-a-car";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
--- includes/process_signup.php
+++ includes/process_signup.php
@@ -1,45 +1,58 @@
 <?php
-
+require("database.php");
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 // name validation
-if (empty($_POST['name'])) {
+if (empty($name)) {
     die('name is required');
 }
 
 // email validation
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    die("Valid email is re  quired");
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
 }
 
 //PASSWORD VALIDATION
 
 //Check if password is at least 8 characters
-if (strlen($_POST["password"]) < 8) {
+if (strlen($password) < 8) {
     die("password must be at least 8 characters");
 }
 
 //Check if password containes at least 1 letter
-if (!preg_match("/[a-z]/i", $_POST["password"])) {
+if (!preg_match("/[a-z]/i", $password)) {
     die('password must contain at least one letter');
 }
 
 //Check if password containes a number
-if (!preg_match("/[0-9]/", $_POST["password"])) {
+if (!preg_match("/[0-9]/", $password)) {
     die('password must contain at least one number');
 }
 
 //Check if password and repeated password are the same
-if ($_POST['password'] !== $_POST["password_confirmation"]) {
+if ($_POST['password'] !== $password) {
     die("Passwords must match");
 }
 
 //Hash the password
-$password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-//Database connection
-$mysqli = require __DIR__ . "/database.php";
 
 // insert user credentials into database table user. 
 
+try {
+    $query = "INSERT INTO user (name, email, password_hash) VALUES ('$name', '$email', '$password_hash')";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // echo $e->getMessage();
+    // echo "<br>";
+    $error_array = $e->errorInfo;
+    echo $error_array[1];
+
+}
 
 //(temporay code)
 // print_r($_POST);
