6b27002eb069593d7f602d6d3b82e41ec4d16ecc
Daan Swart <daswart18@outlook.com>
2023-09-13 09:39:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,54 @@
 include('includes/header.php');
 ?>
 
-<h4 style="text-align: center;">Dit is de homepagina</h4>
+<!-- HERO SECTION -->
+<section id="hero">
+    <div class="slider">
+        <ul class="slides">
+            <li>
+                <img src="includes/img/cars3.png">
+                <div class="caption center-align">
+                    <h2>Auto Huren?</h2>
+                    <a href="login.php" class="btn">Reserveer</a>
+                </div>
+            </li>
+            <li>
+                <img src="includes/img/vans.png">
+                <div class="caption center-align">
+                    <h2>Bus huren?</h2>
+                    <a href="login.php" class="btn">Reserveer</a>
+                </div>
+            </li>
+            <li>
+                <img src="includes/img/contact.png">
+                <div class="caption center-align">
+                    <h2>Vragen?</h2>
+                    <a href="login.php" class="btn">Contact</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<!-- ABOUT SECTION -->
+<div class="section" id="about">
+    <div class="container center">
+            <h3 >Over ons</h3>
+        <p class="flow-text">
+            Lorem ipsum dolor sit amet consectetur adipiscing,
+            elit maecenas consequat diam tristique magna, congue volutpat porta id blandit.
+            Erat habitant nec diam a dictum praesent luctus eros,
+            dictumst litora arcu natoque rutrum pharetra nibh at egestas,
+            feugiat posuere cum nisl blandit et enim. Gravida phasellus pretium feugiat tincidunt a torquent sociosqu,
+            at mauris nibh odio taciti congue arcu mattis, magna cubilia rhoncus augue dapibus neque suscipit,
+            ornare tempus sagittis tempor maecenas dictumst.
+        </p>
+    </div>
+
+</div>
+
+
+
 
 <?php
 include('includes/footer.php');
